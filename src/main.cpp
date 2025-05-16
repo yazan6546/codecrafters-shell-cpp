791b593c57352d7ec630a5db5f4efbda571ea134
yazan6546 <1210145@student.birzeit.edu>
2025-05-16 07:21:36 +0300

    Implement command execution and argument parsing

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <regex>
 #include <string>
 #include <filesystem>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstring>
 
 std::string trim(const std::string &str);
 std::string extract_command(const std::string& input);
@@ -10,6 +13,7 @@ std::string extract_args(const std::string& input);
 void handle_command(const std::string& command, const std::string& args);
 std::string get_env(const std::string &key);
 std::string search_paths(const std::string &paths, const std::string &command);
+char** get_list_of_args(const std::string& args);
 
 [[noreturn]] int main() {
   // Flush after every std::cout / std:cerr
@@ -31,13 +35,13 @@ std::string search_paths(const std::string &paths, const std::string &command);
     }
 
     std::string command = extract_command(input);
+    std::string args = extract_args(input);
 
     if (!is_builtin(command)) {
       std::cout << command <<": not found" << std::endl;
       continue;
     }
 
-    std::string args = extract_args(input);
     handle_command(command, args);
 
   }
@@ -116,4 +120,40 @@ std::string search_paths(const std::string &paths, const std::string &command) {
   }
   return "";
 
+}
+
+void execute_command(const std::string& command, const std::string& args) {
+  pid_t pid = fork();
+  char **list = get_list_of_args(args);
+
+  if (pid == 0) {
+    execvp(command.c_str(), list);
+  }
+  else {
+    waitpid(pid, nullptr, 0);
+  }
+
+  for (int i = 0; list[i] != nullptr; i++) {
+    free(list[i]);  // Free each string
+  }
+
+  delete[] list;
+}
+
+char** get_list_of_args(const std::string& args) {
+  std::vector<std::string> arg_parts;
+  std::istringstream iss(args);
+  std::string arg;
+
+  while (iss >> arg) {
+    arg_parts.push_back(arg);
+  }
+
+  char** array = new char*[arg_parts.size() + 1];
+  for (size_t i = 0; i < arg_parts.size(); i++) {
+    array[i] = strdup(arg_parts[i].c_str());
+  }
+  array[arg_parts.size()] = nullptr; // NULL terminator for execvp
+
+  return array;
 }
