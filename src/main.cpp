7bcc13a835ba8a015a808988caaee6e90ba211d2
yazan6546 <1210145@student.birzeit.edu>
2025-05-16 04:54:52 +0300

    OK change

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,13 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  std::string input;
   // Uncomment this block to pass the first stage
   std::cout << "$ ";
 
-  std::string input;
   std::getline(std::cin, input);
+
+  std::getline(std::cin, input);
+  std::cout << input << ": command not found" << std::endl;
+
 }
